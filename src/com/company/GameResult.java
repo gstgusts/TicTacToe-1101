84cb003edf30930128c5221580f87c321f958acc
Gusts Linkevics <gusts@gst.lv>
2021-01-26 21:34:42 +0200

    Finished solution with console

---
+++ src/com/company/GameResult.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public enum GameResult {
+    INCORRECT_INDEX,
+    CONTINUE,
+    INCORRECT_POSITION,
+    DEUCE,
+    X_WON,
+    O_WON
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var ttt = new TicTacToe();
+	    var scanner = new Scanner(System.in);
+
+	    while (true) {
+	        printBoard(ttt);
+
+            System.out.print("Enter row and column index (row column): ");
+
+            var row = scanner.nextInt();
+            var column = scanner.nextInt();
+
+            System.out.println();
+
+            var result = ttt.placeMark(row-1, column-1);
+
+            if(result == GameResult.X_WON) {
+                System.out.println("X WON !!!");
+                break;
+            } else if(result == GameResult.O_WON) {
+                System.out.println("O WON !!!");
+                break;
+            } else if(result == GameResult.DEUCE) {
+                System.out.println("DEUCE !!!");
+                break;
+            }
+        }
+    }
+
+    public static void printBoard(TicTacToe ttt) {
+       var board = ttt.getBoard();
+
+        System.out.println("---1---2---3--");
+
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(i+1);
+            for (int j = 0; j < board[i].length; j++) {
+                var item = "";
+                if(board[i][j] == FieldType.EMPTY) {
+                    item = "|   ";
+                } else {
+                    item = "| " +board[i][j]+ " ";
+                }
+                System.out.print(item);
+            }
+            System.out.println("|");
+            System.out.println("--------------");
+        }
     }
 }
